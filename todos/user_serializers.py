5eac7df5678fbdf9255d3487bc433ce1d3c90ed8
almat <almatsboom@gmail.com>
2018-08-13 16:51:28 +0600

    Initial commit

---
+++ todos/user_serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(label='ID', read_only=True)
+    first_name = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    last_name = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    
+    class Meta:
+        model = User
+        extra_kwargs = {'password': {'write_only': True}}        
+        fields = ('id', 'email', 'username', 'password', 'first_name', 'last_name')
+    
+    def create(self, validated_data):
+        user = User(
+            email = validated_data.get('email', ''),
+            first_name = validated_data.get('first_name', ''),
+            last_name = validated_data.get('last_name', ''),
+            username = validated_data.get('username')
+        )
+        user.set_password(validated_data.get('password'))
+        user.save()
+        return user
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    last_name = serializers.CharField(required=False, allow_blank=True, max_length=100)
+
+    class Meta:
+        model = User
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }        
+        fields = ('email', 'username', 'first_name', 'last_name')
+    
+    def update(self, user, validated_data):
+        user.username = validated_data.get('username', user.username)
+        user.first_name = validated_data.get('first_name', user.first_name)
+        user.last_name = validated_data.get('last_name', user.last_name)
+        user.email = validated_data.get('email', user.email)
+        user.save()
+        return user
+
+"""
+    """
---
+++ todos/views.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from user_serializers import UserSerializer, UserUpdateSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+
+
+class UserList(APIView):
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({
+                'data': serializer.data,
+                'token': token.key
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLogin(APIView):
+    def post(self, request, format=None):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if username is None or password is None:
+            return Response({'error': 'Please provide both username and password'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        user = authenticate(username=username, password=password)
+        if not user:
+            return Response({'error': 'Invalid Credentials'},
+                            status=status.HTTP_404_NOT_FOUND)
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key},
+                        status=status.HTTP_200_OK)
+
+class Profile(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def put(self, request, format=None):
+        serializer = UserUpdateSerializer(request.user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'data': serializer.data
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request, format=None):
+        serializer = UserSerializer(request.user)
+        return Response({
+            'data': serializer.data
+        })
